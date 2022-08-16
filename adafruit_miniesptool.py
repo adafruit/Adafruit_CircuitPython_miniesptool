dd687733361f21314f6589c893f4075469ef20be
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:15 -0400

    Update version string

--- adafruit_miniesptool.py
+++ adafruit_miniesptool.py
@@ -38,7 +38,7 @@ import time
 import struct
 from digitalio import Direction
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_miniesptool.git"
 
 SYNC_PACKET = b"\x07\x07\x12 UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU"
