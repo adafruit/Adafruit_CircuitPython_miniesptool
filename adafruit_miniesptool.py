ba3c69d5a0bb9de643d949f4aa6acdba470f9694
Brent Rubell <brentru@users.noreply.github.com>
2025-12-01 15:57:03 -0500

    Update adafruit_miniesptool.py
    Co-authored-by: Dan Halbert <halbert@halwitz.org>

--- adafruit_miniesptool.py
+++ adafruit_miniesptool.py
@@ -39,7 +39,7 @@ import time
 
 from digitalio import Direction
 
-__version__ = "0.2.25"
+__version__ = "0.0.0+auto.0"```
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_miniesptool.git"
 
 SYNC_PACKET = b"\x07\x07\x12 UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU"
